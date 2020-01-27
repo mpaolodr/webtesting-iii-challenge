112d011a19add0d151c45f69e4c2acb78841e5b2
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-27 14:54:22 -0500

    created simple test to make sure dashboard is rendering

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
 // Test away
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Dashboard from "./Dashboard";
+
+test("Dashboard renders correctly", () => {
+  render(<Dashboard />);
+});
